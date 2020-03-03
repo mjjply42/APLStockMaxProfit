afd3868f54da29ede65969ae3b302b2f98610e32
matthew-infiswift <mjjply2@gmail.com>
2020-03-02 20:16:45 -0800

    added line graph and animation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
+import { select, line, axisBottom, axisRight, scaleLinear, transition } from 'd3';
+
+const data = [25, 30, 45, 60, 29, 45, 90, 134];
+
+
 
 function App() {
+
+  const svgRef = useRef();
+
+  useEffect(() => {
+    const svg = select(svgRef.current);
+    const xScale = scaleLinear()
+      .domain([0, (data.length - 1)])
+      .range([0 ,400]);
+    const yScale = scaleLinear()
+      .domain([0, 150])
+      .range([300, 0]);
+    const xAxis = axisBottom(xScale)
+      .ticks(data.length - 1)
+      .tickFormat(index => index + 1);
+    svg.select('.x-axis')
+      .style('transform', 'translateY(302px')
+      .call(xAxis);
+
+    const yAxis = axisRight(yScale);
+    svg.select('.y-axis')
+      .style('transform', 'translateX(400px')
+      .call(yAxis);
+
+    const myLine = line()
+      .x((value, index) => xScale(index))
+      .y(yScale);
+
+
+    svg
+      .selectAll('.line')
+      .data([data])
+      .join('path')
+      .attr('class', 'line')
+      .attr('d', myLine)
+      .attr('fill', 'none')
+      .attr('stroke', 'blue');
+    let path = select('.line');
+    const pathLength = path.node().getTotalLength();
+    const transitionPath = transition()
+      .duration(1500);
+
+    path
+      .attr('stroke-dashoffset', pathLength)
+      .attr('stroke-dasharray', pathLength)
+      .transition(transitionPath)
+      .attr('stroke-dashoffset', 0);
+  },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <div style={{backgroundColor: 'grey', width: '400px'}}>
+          <svg width='400' height='300' style={{overflow: 'visible'}} ref={ svgRef }>
+            <g className='x-axis'></g>
+            <g className='y-axis'></g>
+          </svg>
+        </div>
   );
 }
 
